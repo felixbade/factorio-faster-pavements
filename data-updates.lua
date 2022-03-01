cc66d3c499719abecaee938b6b697969d864a3f3
Felix Bade <flix.bade@gmail.com>
2022-03-02 00:03:19 +0200

    Fix mod crashing when space exploration is not installed

--- data-updates.lua
+++ data-updates.lua
@@ -1,1 +1,4 @@
-data.raw["tile"]["rough-stone-path"]["walking_speed_modifier"] = 1.4 -- Space exploration
+-- Space exploration
+if data.raw["tile"]["rough-stone-path"] ~= nil then
+    data.raw["tile"]["rough-stone-path"]["walking_speed_modifier"] = 1.4
+end
